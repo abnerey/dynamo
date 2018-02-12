10ea5c2814e94ba0c0d1be733339c1eeecb6cc70
abnerey <abnerariel-1997@hotmail.com>
2018-02-11 23:18:56 -0600

    master: login tab finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbThemeService } from '@nebular/theme';
+import {
+    NbThemeModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbThemeService,
+    NbCardModule, NbTabsetModule, NbCheckboxModule
+} from '@nebular/theme';
 import { LoginComponent } from './login/login.component';
 import { RoutingModule } from "../routing/routing.module";
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from "../environments/environment";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -18,9 +22,15 @@ import { environment } from "../environments/environment";
   imports: [
       BrowserModule,
       RoutingModule,
+      ReactiveFormsModule,
+      /* Nebular Configuration */
       NbThemeModule.forRoot({name: 'cosmic'}),
       NbLayoutModule,
       NbSidebarModule,
+      NbCardModule,
+      NbTabsetModule,
+      NbCheckboxModule,
+      /* Firebase Configuration */
       AngularFireModule.initializeApp(environment.firebaseConfig),
       AngularFireDatabaseModule,
       AngularFireAuthModule
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, AbstractControl, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'dm-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'dm-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    private username: string;
+    private password: string;
+    private liveSessionId: string;
+    private rememberMe: boolean;
 
-  ngOnInit() {
-  }
+    /* Form Controls */
+    private signInForm: FormGroup;
+    private liveForm: FormGroup;
+    private userControl: AbstractControl;
+    private passControl: AbstractControl;
+    private rememberControl: AbstractControl;
+    private sessionControl: AbstractControl;
 
+    constructor(private readonly formBuilder: FormBuilder,
+                private readonly router: Router) {
+        this.setUpForm();
+    }
+
+    ngOnInit() {
+    }
+
+    setUpForm() {
+        this.signInForm = this.formBuilder.group({
+            userControl: [{value: this.username}, Validators.compose([Validators.required])],
+            passControl: [{value: this.password}, Validators.compose([Validators.required])],
+            rememberControl: [{value: this.rememberMe}, Validators.compose([Validators.required])]
+        });
+        this.userControl = this.signInForm.controls['userControl'].value;
+        this.passControl = this.signInForm.controls['passControl'].value;
+        this.rememberControl = this.signInForm.controls['rememberControl'].value;
+
+        this.liveForm = this.formBuilder.group({
+            sessionControl: [{value: this.liveSessionId}, Validators.compose([Validators.required])]
+        });
+        this.sessionControl = this.liveForm.controls['sessionControl'].value;
+    }
+
+    /* Event Handlers */
+    onSignIn() {
+        console.log('signIn');
+    }
 }
